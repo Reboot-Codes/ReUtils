456389e03346a8ed1029d2b6117cbdac9243dc5d
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-05-27 21:53:36 -0700

    Added welcome message, and disables the default public join and leave messages.

--- src/main/java/com/rebootcodes/reutils/ReUtils.java
+++ src/main/java/com/rebootcodes/reutils/ReUtils.java
@@ -1,11 +1,15 @@
 package com.rebootcodes.reutils;
 
+import com.rebootcodes.reutils.experienceutils.Events;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ReUtils extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        // Register stuff for the ExperienceUtils package
+        getServer().getPluginManager().registerEvents(new Events(), this);
 
         // tell the console that the plugin had been enabled successfully
         getServer().getConsoleSender().sendMessage("[ReUtils] §aPlugin has been enabled!");
