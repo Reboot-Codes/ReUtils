809eac8b5432b3f7b3658b0f5442810c4d052653
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-05-28 11:23:30 -0700

    Fixed the new onPlayerJoin() and onPlayerQuit() events, forgot to add the EventHandler annotation lol

--- src/main/java/com/rebootcodes/reutils/ReUtils.java
+++ src/main/java/com/rebootcodes/reutils/ReUtils.java
@@ -1,6 +1,7 @@
 package com.rebootcodes.reutils;
 
 import com.rebootcodes.reutils.experienceutils.Events;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ReUtils extends JavaPlugin {
@@ -9,10 +10,11 @@ public final class ReUtils extends JavaPlugin {
     public void onEnable() {
 
         // Register stuff for the ExperienceUtils package
+        getServer().getConsoleSender().sendMessage("[ReUtils] " + ChatColor.AQUA + "Registering events for: ExperienceUtils");
         getServer().getPluginManager().registerEvents(new Events(), this);
 
         // tell the console that the plugin had been enabled successfully
-        getServer().getConsoleSender().sendMessage("[ReUtils] §aPlugin has been enabled!");
+        getServer().getConsoleSender().sendMessage("[ReUtils] " + ChatColor.GREEN + "Plugin has been enabled!");
 
     }
 
@@ -20,7 +22,7 @@ public final class ReUtils extends JavaPlugin {
     public void onDisable() {
 
         // Tell the console that the plugin has been disabled successfully
-        getServer().getConsoleSender().sendMessage("[ReUtils] §cPlugin has been disabled!");
+        getServer().getConsoleSender().sendMessage("[ReUtils] " + ChatColor.RED + "Plugin has been disabled!");
 
     }
 
--- src/main/java/com/rebootcodes/reutils/experienceutils/Events.java
+++ src/main/java/com/rebootcodes/reutils/experienceutils/Events.java
@@ -1,28 +1,35 @@
 package com.rebootcodes.reutils.experienceutils;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Events implements Listener {
 
+    @EventHandler
     public void onPlayerJoin (PlayerJoinEvent e) {
 
-        // Set the public join message
-        e.setJoinMessage(null);
-
         // Cast the event into a player object that we can reference.
-        Player player = (Player) e;
+        Player player = e.getPlayer();
+
+        // Set the public join message
+        e.setJoinMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "+" + ChatColor.GOLD + "] " + ChatColor.WHITE + player.getDisplayName());
         // Send the player a message, welcoming them into their server.
-        player.sendMessage("§bWelcome to the server " + player.getName() + "! :D");
+        player.sendMessage(ChatColor.AQUA + "Welcome to the server " + ChatColor.AQUA + ChatColor.BOLD + player.getName() + ChatColor.AQUA + "! :D");
 
     }
 
+    @EventHandler
     public void onPlayerQuit (PlayerQuitEvent e) {
 
+        // Cast the event into a player object that we can reference.
+        Player player = e.getPlayer();
+
         // Set the public join message
-        e.setQuitMessage(null);
+        e.setQuitMessage(ChatColor.GOLD + "[" + ChatColor.RED + "-" + ChatColor.GOLD + "] " + ChatColor.WHITE + player.getDisplayName());
 
     }
 
