4a8b743098d533d0a223ee4abf96756be93d59b5
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-05-28 11:23:30 -0700

    Added Player message

--- src/main/java/com/rebootcodes/reutils/experienceutils/Events.java
+++ src/main/java/com/rebootcodes/reutils/experienceutils/Events.java
@@ -1,5 +1,6 @@
 package com.rebootcodes.reutils.experienceutils;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,20 +10,23 @@ public class Events implements Listener {
 
     public void onPlayerJoin (PlayerJoinEvent e) {
 
-        // Set the public join message
-        e.setJoinMessage(null);
-
         // Cast the event into a player object that we can reference.
-        Player player = (Player) e;
+        Player player = e.getPlayer();
+
+        // Set the public join message
+        e.setJoinMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "+" + ChatColor.GOLD + "] " + ChatColor.WHITE + player.getDisplayName());
         // Send the player a message, welcoming them into their server.
-        player.sendMessage("§bWelcome to the server " + player.getName() + "! :D");
+        player.sendMessage(ChatColor.AQUA + "Welcome to the server " + ChatColor.AQUA + ChatColor.BOLD + player.getName() + ChatColor.AQUA + "! :D");
 
     }
 
     public void onPlayerQuit (PlayerQuitEvent e) {
 
+        // Cast the event into a player object that we can reference.
+        Player player = e.getPlayer();
+
         // Set the public join message
-        e.setQuitMessage(null);
+        e.setQuitMessage(ChatColor.GOLD + "[" + ChatColor.RED + "-" + ChatColor.GOLD + "] " + ChatColor.WHITE + player.getDisplayName());
 
     }
 
